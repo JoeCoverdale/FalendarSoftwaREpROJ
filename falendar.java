2ccfbdba876c12e73bd4a369c31c949c7fe9ac2c
Joseph <joseph2.coverdale@live.uwe.ac.uk>
2022-06-18 21:18:13 +0100

    update 1

--- falendar.java
+++ falendar.java
@@ -3,6 +3,9 @@
 //Student Number: 17024693
 
 //Imported Libraries
+import java.awt.Color;
+
+import org.omg.CORBA.portable.ApplicationException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -11,18 +14,33 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class falendar {
-    
+//Fucntion used within the first page
+public class firstPage extends Application {
+    public void start(Stage Stage){
+        Group root = new Group();
+        Scene scene = new Scene(root, 595, 300, Color.WHITE);
+        stage.setScene(scene);
+        stage.setTitle("Falendar");
+        stage.show();
+    }
+
+//Fucntion used to run program
+    public static void main(String args[]){
+        launch(args);
+    }   
 }
 
+//Create account page function
 public class createAccountPage {
 
 }
 
+//Calendar page function 
 public class calendarPage {
 
 }
 
+//Edit user input function 
 public class editInput {
 
 }
