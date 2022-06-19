6409efba5737e5e66e23f6a0242ecfc846b35688
Joseph <joseph2.coverdale@live.uwe.ac.uk>
2022-06-19 17:48:46 +0100

    Added files

---
+++ src/appOpenPage.java
@@ -0,0 +1,31 @@
+// Software Project: Fitness Calendar
+//Creator: Joseph Coverdale
+//Student Number: 17024693
+
+//Imported Libraries
+import javafx.scene.paint.Color;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+//Function used for first page user will see when using the app
+public class appOpenPage extends Application {
+    public void start(Stage stage){
+        Group root = new Group();
+        Scene scene = new Scene(root, 595,300, Color.WHITE);
+        stage.setScene(scene);
+        stage.setTitle("Falendar");
+        stage.show();
+    }
+    public static void main(String args[]){
+        launch(args);
+    }
+}
+
---
+++ src/calendarPage.java
@@ -0,0 +1,23 @@
+// Software Project: Fitness Calendar
+//Creator: Joseph Coverdale
+//Student Number: 17024693
+
+//Imported Libraries
+import javafx.scene.paint.Color;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+//Fucntion used for calendar page within app
+public class calendarPage {
+    
+}
+
+
